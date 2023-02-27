c47323b847635c0cb8d71189861ce953e1e0caa3
Muhammad Saddam <saddam@climatecamp.io>
2023-02-27 22:43:13 +0500

    initial check in project files

---
+++ EmpowerID.EmployeeManagement/DBContext/DBContext.cs
@@ -0,0 +1,17 @@
+﻿using EmpowerID.EmployeeManagement.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace EmpowerID.EmployeeManagement.DBContext
+{
+    public class EmployeesDbContext : DbContext
+    {
+        protected override void OnConfiguring
+       (DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase(databaseName: "EmployeesDb");
+        }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<EmpowerID.Models.EmployeesViewModel> EmployeesViewModel { get; set; }
+    }
+}
